ed3f4d547d0d552fb491113f8352f991505836f9
Aezakmi <areldikiy@gmail.com>
2023-12-06 23:18:46 +0200

    feat: add terms & privacy

--- views/1/src/components/Footer/index.vue
+++ views/1/src/components/Footer/index.vue
@@ -1,12 +1,92 @@
 <template>
-  <div class="footer">FOOTER</div>
+  <div class="container">
+    <div class="footer">
+      <a-row :gutter="[80, 40]" justify="center">
+        <a-col>
+          <a-flex vertical gap="large">
+            <a-col>
+              <span @click="goTo('settings')">Settings</span>
+            </a-col>
+            <a-col>
+              <span @click="goTo('history')">History</span>
+            </a-col>
+            <a-col>
+              <span @click="goTo(null)">Main</span>
+            </a-col>
+          </a-flex>
+        </a-col>
+        <a-col>
+          <a-flex vertical gap="large">
+            <a-col>
+              <a-badge status="success" title="Success"
+                ><span>Stacking</span></a-badge
+              >
+            </a-col>
+            <a-col>
+              <span>Exchange</span>
+            </a-col>
+            <a-col>
+              <span @click="goTo('wallets')">Wallet</span>
+            </a-col>
+          </a-flex>
+        </a-col>
+        <a-col>
+          <a-flex vertical gap="large">
+            <a-col>
+              <span @click="goTo('about')">About Us</span>
+            </a-col>
+            <a-col>
+              <span @click="goTo('terms')">Terms of use</span>
+            </a-col>
+            <a-col>
+              <span @click="goTo('privacy')">Privacy</span>
+            </a-col>
+          </a-flex>
+        </a-col>
+      </a-row>
+    </div>
+    <div class="reserved">Copyright 2017-2023 © Zalupa All Rights Reserved</div>
+  </div>
 </template>
-<style lang="scss">
+<script>
+import { useRouter } from 'vue-router';
+export default {
+  setup() {
+    const router = useRouter();
+    const goTo = (path = null) => {
+      if (!path) return router.push('/');
+      router.push(`/${path}`);
+    };
+    return {
+      goTo,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  border-top: 1px solid var(--bg-input);
+}
+.reserved {
+  padding-bottom: 20px;
+  text-align: center;
+}
 .footer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 200px;
-  background: var(--bg-base);
+  padding: 70px 0;
+  span {
+    color: var(--text-link);
+    display: flex;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // height: 200px;
+  // background: var(--bg-base);
 }
 </style>
---
+++ views/1/src/views/Terms.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container terms" v-html="t('terms')"></div>
+</template>
+<script>
+import { useI18n } from 'vue-i18n';
+export default {
+  setup() {
+    const { t } = useI18n();
+    return { t };
+  },
+};
+</script>
+<style lang="scss">
+.terms {
+  white-space: pre-line;
+  line-height: 30px;
+  p {
+    font-size: 18px;
+  }
+  h2 {
+    color: var(--text-link);
+  }
+  a {
+    color: var(--bg-purple);
+  }
+}
+</style>
