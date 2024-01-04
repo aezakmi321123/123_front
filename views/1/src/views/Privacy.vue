f93a18a10f9181ab713af7896bba1fda60a04014
Aezakmi <areldikiy@gmail.com>
2024-01-04 18:07:48 +0200

    fx

--- views/1/src/views/Privacy.vue
+++ views/1/src/views/Privacy.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container privacy" v-html="t('privacy')"></div>
+  <div class="container privacy" v-html="text"></div>
 </template>
 <script>
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 export default {
   setup() {
     const { t } = useI18n();
-    return { t };
+    const text = computed(() => t('privacy'));
+    return { t, text };
   },
 };
 </script>
